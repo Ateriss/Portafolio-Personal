0b3f9b4b47ceb85a25f0ea6c17213fcdc54eb8da
Ateriss <alexandralinares2729@gmail.com>
2023-03-09 19:19:20 -0500

    Formulario funcional

--- contact.js
+++ contact.js
@@ -1,39 +1,11 @@
-const form = document.querySelector(".contacto__box");
+const formSubmit = document.querySelector("#formSubmit");
 const nombre = document.querySelector("#name");
 const correo = document.querySelector("#email");
 const sendBtn = document.querySelector(".sendEN");
 const inputs = document.querySelectorAll('.contacto__intro--texto');
 
-form.addEventListener("submit", function(event) {
-  if (!nombre.checkValidity()) {
-    event.preventDefault();
-    nombre.nextElementSibling.innerText =
-      "I need your name to contact you";
-    nombre.nextElementSibling.classList.add("error--activo");
-    nombre.parentElement.classList.add("invalid");
-  } else {
-    nombre.nextElementSibling.classList.remove("error--activo");
-    nombre.parentElement.classList.remove("invalid");
-  }
-
-  if (!correo.checkValidity()) {
-    event.preventDefault();
-    if (correo.validity.valueMissing) {
-      correo.nextElementSibling.innerText =
-        "I need your email to contact you";
-    } else {
-      correo.nextElementSibling.innerText =
-        "Enter a valid email, for example example@mail.com";
-    }
-    correo.nextElementSibling.classList.add("error--activo");
-    correo.parentElement.classList.add("invalid");
-  } else {
-    correo.nextElementSibling.classList.remove("error--activo");
-    correo.parentElement.classList.remove("invalid");
-  }
-});
-
 sendBtn.addEventListener("click", function(event) {
+  event.preventDefault();
   if (!nombre.checkValidity()) {
     nombre.nextElementSibling.innerText =
       "I need your name to contact you";
@@ -58,20 +30,36 @@ sendBtn.addEventListener("click", function(event) {
     correo.nextElementSibling.classList.remove("error--activo");
     correo.parentElement.classList.remove("invalid");
   }
-});
 
 
-sendBtn.addEventListener('click', () => {
+  });
 
+
+  sendBtn.addEventListener("click", ()=>  {
   if (nombre.validity.valid && correo.validity.valid) {
-    inputs.forEach((input) => (input.value = ''));
-    msj.value = '';
-    form.reset();
-    setTimeout(() => swal({
-      title: "Thanks!",
-      text: "I will contact you as soon as possible",
-      icon: "success",
-      button: "Aww <3",
-    }), 500);
-  }
-});
+    const formData = new FormData(formSubmit);
+    fetch("https://formsubmit.co/ajax/alexandralinares2729@gmail.com", {
+      method: "POST",
+      headers: { "Content-Type": "application/json",  'Accept': 'application/json' },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    })
+      inputs.forEach((input) => (input.value = ""));
+      msj.value = "";
+      formSubmit.reset();
+      setTimeout(
+        () =>
+          swal({
+            title: "¡Gracias!",
+            text: "Me contactaré contigo lo antes posible",
+            icon: "success",
+            button: "Aww <3",
+          }),
+        500
+      )
+      
+    
+      .then(response =>  response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+        
+        }})
--- contacto.js
+++ contacto.js
@@ -1,39 +1,11 @@
-const form = document.querySelector(".contacto__box");
+const formSubmit = document.querySelector("#formSubmit");
 const nombre = document.querySelector("#nombre");
 const correo = document.querySelector("#correo");
 const sendBtn = document.querySelector(".sendES");
 const inputs = document.querySelectorAll('.contacto__intro--texto');
 
-form.addEventListener("submit", function(event) {
-  if (!nombre.checkValidity()) {
-    event.preventDefault();
-    nombre.nextElementSibling.innerText =
-      "Necesito tu nombre para comunicarme contigo";
-    nombre.nextElementSibling.classList.add("error--activo");
-    nombre.parentElement.classList.add("invalid");
-  } else {
-    nombre.nextElementSibling.classList.remove("error--activo");
-    nombre.parentElement.classList.remove("invalid");
-  }
-
-  if (!correo.checkValidity()) {
-    event.preventDefault();
-    if (correo.validity.valueMissing) {
-      correo.nextElementSibling.innerText =
-        "Necesito tu E-mail para comunicarme contigo";
-    } else {
-      correo.nextElementSibling.innerText =
-        "Ingresa un correo válido, como example@mail.com";
-    }
-    correo.nextElementSibling.classList.add("error--activo");
-    correo.parentElement.classList.add("invalid");
-  } else {
-    correo.nextElementSibling.classList.remove("error--activo");
-    correo.parentElement.classList.remove("invalid");
-  }
-});
-
 sendBtn.addEventListener("click", function(event) {
+  event.preventDefault();
   if (!nombre.checkValidity()) {
     nombre.nextElementSibling.innerText =
       "Necesito tu nombre para comunicarme contigo";
@@ -58,20 +30,36 @@ sendBtn.addEventListener("click", function(event) {
     correo.nextElementSibling.classList.remove("error--activo");
     correo.parentElement.classList.remove("invalid");
   }
-});
 
 
-sendBtn.addEventListener('click', () => {
+  });
 
+
+  sendBtn.addEventListener("click", ()=>  {
   if (nombre.validity.valid && correo.validity.valid) {
-    inputs.forEach((input) => (input.value = ''));
-    msj.value = '';
-    form.reset();
-    setTimeout(() => swal({
-      title: "¡Gracias!",
-      text: "Me contactaré contigo lo antes posible",
-      icon: "success",
-      button: "Aww <3",
-    }), 500);
-  }
-});
+    const formData = new FormData(formSubmit);
+    fetch("https://formsubmit.co/ajax/alexandralinares2729@gmail.com", {
+      method: "POST",
+      headers: { "Content-Type": "application/json",  'Accept': 'application/json' },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    })
+      inputs.forEach((input) => (input.value = ""));
+      msj.value = "";
+      formSubmit.reset();
+      setTimeout(
+        () =>
+          swal({
+            title: "¡Gracias!",
+            text: "Me contactaré contigo lo antes posible",
+            icon: "success",
+            button: "Aww <3",
+          }),
+        500
+      )
+      
+    
+      .then(response =>  response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+        
+        }})
